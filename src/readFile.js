17e3747c37f3b338b1de6a295506f87037d5b9cc
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-10-23 17:13:00 -0300

    reading text, url ans files path

--- src/readFile.js
+++ src/readFile.js
@@ -1,65 +1,45 @@
 const chalk = require('chalk');
 const fs = require('fs');
-// const path = require('path');
-
-// module.exports = function (dir, filter, callback) { 
-
-//     fs.readdir(dir, function (err, list) {
-//         if(err) {
-//             return callback(err);
-//         }
-//         list = list.filter(function (file) {
-//             return path.extname(file) === '.' + filter; 
-//         })
-//         callback(null, list)
-//     })
-// }
-
-// const getFile = (pathFile) => {
-//     return new Promise(function promiseResolve(resolve, reject) {
-//         fs.readFile(pathFile, 'utf-8', (error, text) => { 
-//             if (error) {
-//                 error = 'ERROR: Invalid file.';
-//                 return reject(err);
-//             } else {
-//                 const regex = data.match(/\[([^\[]+)\](\(http.*?\))/gm); 
-//                 console.log(chalk.green(text)) 
-
-//             }
-//         }) 
-//     }) 
-    
-// }
+const path = require('path');
+const https = require('https');
+const http = require('http');
 
 function throwError(err) {
     throw new Error(chalk.red(err.code, 'there is no file in this path')); 
 }
 
-async function getFile (pathFile) {
-    try {
-        const text = await fs.promises.readFile(pathFile, 'utf8');
-        console.log(chalk.green(text));
-    } catch (err) {
-        throwError(err);
-    }
+function readFile (file) {
+    return new Promise((resolved, rejected) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                rejected(throwError(err));
+            }
+            const regex = data.match(/\[([^\]]*)\]+\((https?:\/\/[^\[\s\)]*)/gm);
+            // console.log(regex);
+            regex.map((i) => {
+                const strLink = i.replace('[', '').replace('\n', '');
+                const splitToArray = strLink.split('](');
+                const resultText = { 
+                    text: splitToArray[0], 
+                    href: splitToArray[1], 
+                    file
+                };
+                return resolved(console.log(resultText));
+            })
+        });
+    });
 }
 
-// const getFile = (pathFile) => {
-//     fs.promises
-//     .readFile(pathFile, 'utf8')
-//     .then((text) => console.log(text))
-//     .catch((err) => throwError(err))
-// }
+readFile('./text.md', console.log(chalk.blueBright('success in get file')))
 
-// const getFile = (pathFile) => {
-//     fs.readFile(pathFile, 'utf-8', (err, text) => { 
-//         if (err) {
-//             throwError(err); 
-//         }
-//         console.log(chalk.blueBright(text));
-//     }) 
-// }
 
-getFile('./', console.log(chalk.blueBright('success in get file')))
+// async function getFile (pathFile) {
+//     try {
+//         const text = await fs.promises.readFile(pathFile, 'utf8');
+//         console.log(chalk.green(text));
+//     } catch (err) {
+//         throwError(err);
+//     }
+// }
 
 
